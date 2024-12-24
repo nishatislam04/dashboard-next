38cfa91a652ddde46c3f9c2a633a3d801b8cdf59
nishatislam04 <nishatislam3108@gmail.com>
2024-12-25 04:53:46 +0600

    test

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -32,23 +32,11 @@ export default function CountChart() {
 			{/* CHART */}
 			<div className="relative w-full h-[75%]">
 				<ResponsiveContainer>
-					<RadialBarChart
-						cx="50%"
-						cy="50%"
-						innerRadius="40%"
-						outerRadius="100%"
-						barSize={32}
-						data={data}>
+					<RadialBarChart cx="50%" cy="50%" innerRadius="40%" outerRadius="100%" barSize={32} data={data}>
 						<RadialBar background dataKey="count" />
 					</RadialBarChart>
 				</ResponsiveContainer>
-				<Image
-					src="/maleFemale.png"
-					alt=""
-					width={50}
-					height={50}
-					className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-				/>
+				<Image src="/maleFemale.png" alt="" width={50} height={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
 			</div>
 			{/* BOTTOM */}
 			<div className="flex justify-center gap-16">
