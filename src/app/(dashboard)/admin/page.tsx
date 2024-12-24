32392e39c116ee6f82e29288ffad3c870c38fd1e
nishatislam04 <nishatislam3108@gmail.com>
2024-12-25 02:45:11 +0600

    add: admin page userCard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,21 @@
+import UserCard from "@/components/UserCard";
+
 export default function AdminPage() {
-  return (
-    <div>AdminPage</div>
-  );
+	return (
+		<div className="p-4 flex gap-4 flex-col md:flex-row">
+			{/* left */}
+			<div className="w-full lg:w-2/3">
+				{/* user card */}
+				<div className="flex gap-4 justify-between flex-wrap">
+					<UserCard type="student" />
+					<UserCard type="teacher" />
+					<UserCard type="parent" />
+					<UserCard type="staff" />
+				</div>
+			</div>
+
+			{/* right */}
+			<div className="w-full lg:w-1/3">r</div>
+		</div>
+	);
 }
