4a5028fe19a90dca44586428381a459bf7084edb
nishatislam04 <nishatislam3108@gmail.com>
2024-12-25 07:01:10 +0600

    add: second line-chart

---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Image from "next/image";
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+
+const data = [
+	{
+		name: "Mon",
+		present: 60,
+		absent: 40,
+	},
+	{
+		name: "Tue",
+		present: 30,
+		absent: 40,
+	},
+	{
+		name: "Wed",
+		present: 90,
+		absent: 20,
+	},
+	{
+		name: "Thu",
+		present: 45,
+		absent: 23,
+	},
+	{
+		name: "Fri",
+		present: 100,
+		absent: 10,
+	},
+];
+
+export default function AttendanceChart() {
+	return (
+		<div className="bg-white rounded-lg p-4 h-full">
+			<div className="flex justify-between items-center">
+				<h1 className="text-lg font-semibold">Attendance</h1>
+				<Image src="/moreDark.png" alt="" width={20} height={20} />
+			</div>
+			<ResponsiveContainer width="100%" height="90%">
+				<BarChart width={500} height={300} data={data} barSize={20}>
+					<CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
+					<XAxis dataKey="name" axisLine={false} tick={{ fill: "#d1d5db" }} tickLine={false} />
+					<YAxis axisLine={false} tick={{ fill: "#d1d5db" }} tickLine={false} />
+					<Tooltip contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }} />
+					<Legend align="left" verticalAlign="top" wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }} />
+					<Bar dataKey="present" fill="#fae27c" legendType="circle" radius={[10, 10, 0, 0]} />
+					<Bar dataKey="absent" fill="#c3ebfa" legendType="circle" radius={[10, 10, 0, 0]} />
+				</BarChart>
+			</ResponsiveContainer>
+		</div>
+	);
+}
