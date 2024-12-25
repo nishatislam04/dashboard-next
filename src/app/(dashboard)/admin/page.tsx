7b9dea80e1b8d4d14bce89694e814142a505d22e
nishatislam04 <nishatislam3108@gmail.com>
2024-12-25 07:15:12 +0600

    add: bottom chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 export default function AdminPage() {
@@ -27,7 +28,9 @@ export default function AdminPage() {
 					</div>
 				</div>
 				{/* bottom chart */}
-				<div className="w-full h-[500px]"></div>
+				<div className="w-full h-[500px]">
+					<FinanceChart />
+				</div>
 			</div>
 
 			{/* right */}
